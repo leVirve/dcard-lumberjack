9acb93691378c42896f8ca989b65df42a98d56a6
Salas <gae.m.project@gmail.com>
2016-08-10 09:05:06 +0800

    add lumberjack basic module

---
+++ lumberjack/crawler.py
@@ -0,0 +1,29 @@
+import time
+import logging
+
+
+from dcard import Dcard
+
+
+logger = logging.getLogger(__name__)
+
+
+def crawl(
+        name,
+        boundary_num=None,
+        before_id=None,
+        boundary_date=None,
+        callback=None):
+
+    dcard = Dcard()
+
+    s = time.time()
+    result = dcard.forums(name).get_metas(
+            num=boundary_num if boundary_num else dcard.forums.infinite_page,
+            before=before_id if before_id else None,
+            timebound=boundary_date.isoformat() if boundary_date else '',
+            callback=lambda metas, name=name: callback(metas, name)
+        )
+
+    logger.debug('<%s> used %.05f sec.', name, time.time() - s)
+    return result
---
+++ lumberjack/strategy.py
@@ -0,0 +1,59 @@
+import logging
+
+from pymongo import MongoClient
+
+logger = logging.getLogger(__name__)
+
+
+class DBStrategy:
+
+    client = MongoClient(connect=False)
+    db = client['dcard-metas']
+
+    @staticmethod
+    def insert_metas(metas, collect, forum):
+        collect = DBStrategy.db[forum]
+        result = collect.insert_many(metas)
+        logger.info('[db] #Forum %s: %d', forum, len(result.inserted_ids))
+        return result
+
+    @staticmethod
+    def upsert_metas(metas, forum):
+        collect = DBStrategy.db[forum]
+        bulk = collect.initialize_unordered_bulk_op()
+        for meta in metas:
+            bulk.find({'id': meta['id']}).upsert().replace_one(meta)
+        result = bulk.execute() if len(metas) else None
+        if result:
+            del result['upserted']
+        logger.info('[db] #Forum %s: %s', forum, result)
+        return result
+
+    @staticmethod
+    def upsert_metas_if_newer(metas, forum):
+        collect = DBStrategy.db[forum]
+        bulk = collect.initialize_unordered_bulk_op()
+        for meta in metas:
+            bulk.find({
+                'id': meta['id'],
+                'updatedAt': {'$gt': meta['updatedAt']}
+            }).upsert().replace_one(meta)
+        result = bulk.execute() if len(metas) else None
+        if result:
+            del result['upserted']
+        logger.info('[db] #Forum %s: %s', forum, result)
+        return result
+
+    @staticmethod
+    def find_older_metas(metas, forum):
+        collect = DBStrategy.db[forum]
+        result = [
+            collect.find_one({
+                'id': meta['id'],
+                'updatedAt': {'$gt': meta['updatedAt']}
+            })
+            for meta in metas
+        ]
+        ret = [(r['_id'], meta) for r, meta in zip(result, metas) if r]
+        logger.info('[db] #Forum %s: %d items need update', forum, len(ret))
+        return ret
