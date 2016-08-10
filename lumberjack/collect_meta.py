08a8a1cc0cabdeada7c0e0814c9e0f45467b6c32
Salas <gae.m.project@gmail.com>
2016-08-10 09:41:33 +0800

    there should not need a crawler

--- lumberjack/collect_meta.py
+++ lumberjack/collect_meta.py
@@ -1,13 +1,39 @@
+import time
+import logging
 
+from dcard import Dcard
 
-from lumberjack.crawler import crawl
 from lumberjack.strategy import DBStrategy
 
 
+logger = logging.getLogger(__name__)
+
+
+def collecter(
+        name,
+        boundary_num=None,
+        before_id=None,
+        boundary_date=None,
+        callback=None):
+
+    dcard = Dcard()
+
+    s = time.time()
+    result = dcard.forums(name).get_metas(
+            num=boundary_num if boundary_num else dcard.forums.infinite_page,
+            before=before_id if before_id else None,
+            timebound=boundary_date.isoformat() if boundary_date else '',
+            callback=lambda metas, name=name: callback(metas, name)
+        )
+
+    logger.debug('<%s> used %.05f sec.', name, time.time() - s)
+    return result
+
+
 def collect_all(forum):
-    return crawl(forum, callback=DBStrategy.insert_metas)
+    return collecter(forum, callback=DBStrategy.insert_metas)
 
 
 def collect(bundle):
     forum, param = bundle
-    return crawl(forum, **param, callback=DBStrategy.upsert_metas)
+    return collecter(forum, **param, callback=DBStrategy.upsert_metas)
