094abb5d205f2f5f9c19abb32cfced06d621748f
Salas <gae.m.project@gmail.com>
2016-08-12 20:13:06 +0800

    add funcs on get_posts

--- lumberjack/strategy.py
+++ lumberjack/strategy.py
@@ -1,6 +1,7 @@
 import logging
 
 from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 logger = logging.getLogger(__name__)
 
@@ -34,6 +35,7 @@ class DBStrategy:
         collect = DBStrategy.db[forum]
         bulk = collect.initialize_unordered_bulk_op()
         for meta in metas:
+            meta['pending'] = True
             bulk.find({
                 'id': meta['id'],
                 'updatedAt': {'$gt': meta['updatedAt']}
@@ -45,15 +47,22 @@ class DBStrategy:
         return result
 
     @staticmethod
-    def find_older_metas(metas, forum):
+    def find_pending_metas(forum):
         collect = DBStrategy.db[forum]
-        result = [
-            collect.find_one({
-                'id': meta['id'],
-                'updatedAt': {'$gt': meta['updatedAt']}
-            })
-            for meta in metas
-        ]
-        ret = [(r['_id'], meta) for r, meta in zip(result, metas) if r]
-        logger.info('[db] #Forum %s: %d items need update', forum, len(ret))
-        return ret
+        return collect.find({'pending': True})
+
+    @staticmethod
+    def change_pending_meta(forum, meta):
+        collect = DBStrategy.db[forum]
+        collect.update_one(
+            {'_id': ObjectId(meta['_id'])},
+            {'$set': {'pending': False}})
+
+
+class Datastore:
+
+    client = MongoClient(connect=False)
+    db = client['dcard-posts']
+
+    def save(self, post):
+        self.db.posts.insert(post)
--- spider.py
+++ spider.py
@@ -1,10 +1,8 @@
-import time
 import logging
 import datetime
-from multiprocessing.dummy import Pool
 
 from dcard import Dcard
-from lumberjack.collect_meta import collect, collect_all
+from lumberjack.tasks import collect, collect_all, collect_posts
 
 
 logger = logging.getLogger('lumberjack')
@@ -24,31 +22,21 @@ dcard = Dcard()
 forums = dcard.forums.get(no_school=True)
 
 
-def brute(forums):
-    with Pool(processes=2) as pool:
-        result = pool.map(collect, forums)
-    return result
-
-
 def get_all_metas(forum_name):
     ''' Run this once, and all metas will insert into database. '''
-    return collect_all(forum_name)
+    return collect_all.delay(forum_name)
 
 
 def get_metas_in_one_weeks(forum_name):
     ''' Run this every week, and metas will upsert into database. '''
-    time_limitation = datetime.datetime.utcnow() - datetime.timedelta(weeks=1)
+    time_limitation = datetime.datetime.utcnow() - datetime.timedelta(hours=1)
     bundle = (
         forum_name,
         {'boundary_date': time_limitation}
     )
-    return collect(bundle)
+    return collect.delay(bundle)
 
 
 if __name__ == '__main__':
-    s = time.time()
-
-    result = get_metas_in_one_weeks('pokemon')
-    print(result)
-
-    logger.info('Total Work: {:.05} sec'.format(time.time() - s))
+    get_metas_in_one_weeks('pokemon')
+    collect_posts('pokemon')
