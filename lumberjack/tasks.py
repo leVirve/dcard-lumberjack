ab86a84e5b89abff011bb44a27e5f8ed677dc7a1
Salas <gae.m.project@gmail.com>
2016-08-12 20:51:19 +0800

    refactor and minimize structure

--- lumberjack/tasks.py
+++ lumberjack/tasks.py
@@ -1,19 +1,28 @@
+import time
+import logging
+
+from dcard import Dcard
 from lumberjack.celery import app
-from lumberjack.collector import collect_metas, collect_posts
-from lumberjack.strategy import Datastore
+from lumberjack.datastore import Datastore
+
+
+logger = logging.getLogger(__name__)
+dcard = Dcard()
 
 
 @app.task
 def collect_meta_task(bundle):
     forum, param = bundle
-    return collect_metas(
-        forum, **param, callback=Datastore.upsert_metas_if_newer)
+    param.setdefault('num', dcard.forums.infinite_page)
+    return dcard.forums(forum) \
+                .get_metas(**param, callback=Datastore.upsert_metas_if_newer)
 
 
 @app.task
 def collect_all_metas_task(forum):
-    return collect_metas(
-        forum, callback=Datastore.insert_metas)
+    return dcard.forums(forum).get_metas(
+        num=dcard.forums.infinite_page,
+        callback=Datastore.insert_metas)
 
 
 @app.task
@@ -23,6 +32,6 @@ def collect_posts_task(forum):
         {'_id': m['_id'], 'id': m['id'], 'commentCount': m['commentCount']}
         for m in db.find_pending_metas(forum)
     ]
-    for task, post in zip(tasks, collect_posts(tasks)):
+    for task, post in zip(tasks, dcard.posts(tasks).get()):
         db.finish_pending_meta(forum, task)
         db.save(post)
